b68c5cb66d4688ae9f74d32e697c4d3fda5f0983
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-03-23 04:35:59 -0300

    nav component maded & nav component responsiviness maded

---
+++ components/Nav.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+import moonIcon from '@/assets/moon-icon.svg'
+
+const Nav = () => {
+   return (
+      <nav className='lateral-spacing flex justify-between items-center py-8 border border-b-1 border-gray-300 border-solid'>
+         <h1 className='main-title'>Where in the world?</h1>
+
+         <button className='flex items-center gap-1 sm:gap-2 md:gap-3 cursor-pointer'>
+            <Image 
+               src={moonIcon}
+               width={20}
+               height={20}
+               className='image'
+            />
+            <h3 className='font-bold toggle-text'>Dark Mode</h3>
+         </button>
+      </nav>
+   )
+}
+
+export default Nav
